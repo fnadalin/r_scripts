d054d4c70b432c4c26c30a8773c31a477fc141f0
Francesca Nadalin <francesca@ebi.ac.uk>
2023-05-26 17:15:13 +0100

    color change

--- gene_expression_analysis/Seurat_plot.R
+++ gene_expression_analysis/Seurat_plot.R
@@ -24,7 +24,7 @@ DrPlot <- function(object, dr = "pca", dr_plot = "pca", pcs = 50, pval = 1e-05, 
 	dev.off()
 	
 	pdf(file.path(out_dir, paste0(dr_type, "_plot_UMIcount.pdf")), width = 3.7*2, height = 3*2)
-	print(FeaturePlot(object, features = "nCount_RNA", reduction = dr_plot))
+	print(FeaturePlot(object, features = "nCount_RNA", reduction = dr_plot, cols = c("blue","red")))
 	dev.off()
 	
 	g <- ggplot(data = object@meta.data, aes(x = nCount_RNA)) + theme_classic() + geom_density() + ylab("density")
